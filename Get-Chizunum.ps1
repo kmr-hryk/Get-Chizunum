b62ee41c3589ee9d567c160437a63ab032dbec09
Hiroyuki Kimura <z_kimura@k-side.net>
2014-11-14 13:47:11 +0900

    まとめた行から不要なカラムを省く

--- Get-Chizunum.ps1
+++ Get-Chizunum.ps1
@@ -1,8 +1,25 @@
 $Filename = $ARGS[0]
+$Chisekiatr = ""
+$Chizunum =""
+
 $Filename = $(Get-ChildItem $Filename).FullName
 
-$Chisekiatr = Get-Content $Filename | ForEach-Object { if ($_ -match "^[0-9]") { Write-Output "`r`n$_" } else { Write-Output ",$_" } }
+$Chisekiatr = Get-Content $Filename | ForEach-Object {
+   if ($_ -match "^[0-9]") {
+      Write-Output "`r`n$_"
+   } else {
+      Write-Output ",$_"
+   }
+}
 
-$Chisekiatr = $Chisekiatr -join ""
+$Chisekiatr = $Chisekiatr -join "" -Split "`r`n"
 
-Write-Output $Chisekiatr[2]
+Write-Output $Chisekiatr | ForEach-Object {
+   $tmp = $_.Split(",")
+   $Chizunum = $tmp[2],$tmp[4]
+   for ( $i=21 ; $i -le $tmp.Length ; $i++) {
+      $Chizunum += $tmp[$i]
+   }
+   $Chizunum = $Chizunum -join ","
+   Write-Output $Chizunum
+}
