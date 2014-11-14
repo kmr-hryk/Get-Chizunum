c0577da4e67db938695c108cb384cee5632b0f0a
Hiroyuki Kimura <z_kimura@k-side.net>
2014-11-14 14:35:36 +0900

    整形済CSVファイル出力

--- Get-Chizunum.ps1
+++ Get-Chizunum.ps1
@@ -1,7 +1,7 @@
 [reflection.assembly]::LoadWithPartialName("Microsoft.VisualBasic") > $null
 $vbnarrow = [Microsoft.VisualBasic.VbStrConv]::Narrow
 $Filename = $ARGS[0]
-$Chizuatr
+$Chizuatr = @()
 
 $Filename = $(Get-ChildItem $Filename).FullName
 
@@ -17,19 +17,20 @@ $Chisekiatr = $Chisekiatr -join "" -Split "`r`n"
 
 Write-Output $Chisekiatr | ForEach-Object {
    $tmp = $_.Split(",")
-   $Chizunum = $tmp[2],[Microsoft.VisualBasic.Strings]::StrConv($tmp[4],$vbnarrow)
-   for ( $i=21 ; $i -le $tmp.Length ; $i++) {
-      $tmpnum = [Microsoft.VisualBasic.Strings]::StrConv($tmp[$i],$vbnarrow)
-      if ( $tmpnum.Length -ne 0 ) {
-         $idxnum = $tmpnum.IndexOf("0")
-         $mae = $tmpnum.SubString(0,$idxnum)
-         $soe = $tmpnum.SubString($idxnum + 1,1)
-         $ban = $tmpnum.SubString($idxnum + 2)
-         $Chizunum += $mae + $soe + " " + $ban
+   if ( $_.Length -ne 0 ) {
+      $Chizunum = $tmp[2],[Microsoft.VisualBasic.Strings]::StrConv($tmp[4],$vbnarrow)
+      for ( $i=21 ; $i -le $tmp.Length ; $i++) {
+         $tmpnum = [Microsoft.VisualBasic.Strings]::StrConv($tmp[$i],$vbnarrow)
+         if ( $tmpnum.Length -ne 0 ) {
+            $idxnum = $tmpnum.IndexOf("0")
+            $mae = $tmpnum.SubString(0,$idxnum)
+            $soe = $tmpnum.SubString($idxnum + 1,1)
+            $ban = $tmpnum.SubString($idxnum + 2)
+            $Chizunum += $mae + $soe + " " + $ban
+         }
       }
+      $Chizuatr += $Chizunum -join ","
    }
-   $Chizuatr += $Chizunum -join ","
 }
 
-Write-Output $Chizuatr
-#| Out-File $Filename.Replace(".atr",".csv")
+Write-Output $Chizuatr | Out-File $Filename.Replace(".atr",".csv")
