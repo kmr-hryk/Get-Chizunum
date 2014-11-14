de69050369b6d9521da9261501304ce9e921d546
Hiroyuki Kimura <z_kimura@k-side.net>
2014-11-14 13:15:02 +0900

    属性と地図番号を1行にまとめるところまで

--- Get-Chizunum.ps1
+++ Get-Chizunum.ps1
@@ -1,6 +1,8 @@
 $Filename = $ARGS[0]
 $Filename = $(Get-ChildItem $Filename).FullName
 
-$Chisekiatr = [System.IO.File]::ReadAllText($Filename)
+$Chisekiatr = Get-Content $Filename | ForEach-Object { if ($_ -match "^[0-9]") { Write-Output "`r`n$_" } else { Write-Output ",$_" } }
+
+$Chisekiatr = $Chisekiatr -join ""
 
 Write-Output $Chisekiatr
